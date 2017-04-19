05c978fdb1b02fbdb29c5cd23a796a22b2b5f010
jameslNeurio <james.leong@neur.io>
2017-04-18 21:09:50 -0700

    routes and navigation added

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Countdown = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <p>Countdown.jsx component</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Timer = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <p>Timer.jsx component</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Timer;
