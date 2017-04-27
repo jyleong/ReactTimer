34bcb6966e4e3db41b5c42defaac413d021b064d
jameslNeurio <james.leong@neur.io>
2017-04-26 21:23:42 -0700

    added timer tests

---
+++ app/tests/components/Timer.test.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Timer = require('Timer');
+
+describe('Timer', ()=> {
+    it('should exist', () => {
+        expect(Timer).toExist();
+    });
+
+    it('should start timer on started status', (done) => {
+        var timer = TestUtils.renderIntoDocument(<Timer/>);
+
+        timer.handleStatusChange('started');
+        expect(timer.state.count).toBe(0);
+
+        setTimeout(() => {
+            expect(timer.state.timerStatus).toBe('started');
+            expect(timer.state.count).toBe(1);
+            done();
+        },1001);
+    });
+
+    it('should pause timer on paused status', (done)=> {
+        var timer = TestUtils.renderIntoDocument(<Timer/>);
+        timer.handleSetCountdown(3);
+        timer.handleStatusChange('paused');
+
+        setTimeout(() => {
+            expect(timer.state.count).toBe(3);
+            expect(timer.state.timerStatus).toBe('paused');
+            done();
+        },1001);
+    });
+
+});
