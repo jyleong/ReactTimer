b1bddab5698d20c1c82ee96d552f8ae478db8d06
jameslNeurio <james.leong@neur.io>
2017-04-26 21:08:22 -0700

    timer component made

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,62 @@
 var React = require('react');
-var {Link} = require('react-router');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+    getInitialState: function() {
+      return {
+        count: 0,
+        timerStatus: 'paused'
+      };
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+        if (this.state.timerStatus !== prevState.timerStatus) {
+            switch(this.state.timerStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'stopped':
+                    this.setState({count:0});
+                case 'paused':
+                    clearInterval(this.timer);
+                    this.timer= undefined;
+                    break;
+            }
+        }
+    },
+    componentWillUnmount: function() {
+        console.log('componentwillunmount');
+        //want to clear the interval
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+    startTimer: function() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count+1;
+            this.setState({
+                count: newCount
+            });
+
+        }, 1000);
+    },
+    handleSetCountdown: function(seconds) {
+        this.setState({
+            count: seconds,
+            timerStatus: 'started'
+        });
+    },
+    handleStatusChange: function(newStatus) {
+        this.setState({
+            timerStatus: newStatus
+        });
+    },
     render: function() {
+        var {count, timerStatus} = this.state;
         return (
             <div>
-                <p>Timer.jsx component</p>
+                <h1 className="page-title">Timer App</h1>
+                <Clock totalSeconds={count}/>
+                <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
             </div>
         );
     }
